98d7ea947d2592dfdfe37857e2c1007864d8b3a2
AdamMusa <adammusa2222@gmail.com>
2021-05-12 02:26:58 +0100

    added provider for resourcing my project

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
+import cv2
 import numpy as np
-import cv2 as cv
 
-cap = cv.VideoCapture(0)
-    if not cap.isOpened():
-        print("Cannot open camera")
-        exit()
-    while True:
-        # Capture frame-by-frame
+cap = cv2.VideoCapture(0)
+
+def empty(a):
+    pass
+while True:
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # cv2.putText(frame,"OpenCV",(300,200),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+    cv2.namedWindow("Hello")
+    cv2.resizeWindow("Hello",(200,300))
+    cv2.createTrackbar("Min","Hello",1,190,empty)
+    cv2.imshow('frame',gray)
 
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('q'):
+    if cv2.waitKey(1)== ord("q"):
         break
 
-# When everything done, release the capture
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
